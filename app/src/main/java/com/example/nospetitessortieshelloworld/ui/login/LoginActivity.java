455a4622509ace5bdade8a0749ee7d34063b7b7c
Benjamin <benjchauvet@gmail.com>
2022-12-27 23:56:07 +0100

    Naive modification to prevent an early exit

--- app/src/main/java/com/example/nospetitessortieshelloworld/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/nospetitessortieshelloworld/ui/login/LoginActivity.java
@@ -78,10 +78,10 @@ public class LoginActivity extends AppCompatActivity {
                 if (loginResult.getSuccess() != null) {
                     updateUiWithUser(loginResult.getSuccess());
                 }
-                setResult(Activity.RESULT_OK);
+                //setResult(Activity.RESULT_OK);
 
                 //Complete and destroy login activity once successful
-                finish();
+                //finish();
             }
         });
 
